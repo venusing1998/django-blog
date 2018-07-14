e6cb5f654b63c1c7a51eb93b44355648fad9e622
Chris <christoleavenow@gmail.com>
2018-07-14 10:56:44 +0800

    modify djangoblog,sh

--- djangoblog.sh
+++ djangoblog.sh
@@ -1,4 +1,4 @@
 cp uwsgi.ini /etc/nginx/uwsgi.ini
-cp nginx.conf /etc/nginx//nginx.conf
+cp nginx.conf /etc/nginx/nginx.conf
 uwsgi --ini /etc/nginx/uwsgi.ini
-nginx -c /etc/nginx//nginx.conf
+nginx -c /etc/nginx/nginx.conf

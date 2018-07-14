3c5299a1d57552ffeac834b41b166a629a4b4510
Chris <christoleavenow@gmail.com>
2018-07-14 12:29:32 +0800

    update

--- djangoblog.sh
+++ djangoblog.sh
@@ -2,4 +2,5 @@ cp uwsgi.ini /usr/local/nginx/conf/uwsgi.ini
 mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.bak
 cp nginx.conf /usr/local/nginx/conf/nginx.conf
 uwsgi --ini /usr/local/nginx/conf/uwsgi.ini
+pkill nginx
 nginx -c /usr/local/nginx/conf/nginx.conf
